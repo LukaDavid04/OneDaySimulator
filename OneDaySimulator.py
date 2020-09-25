2dda5303d9dbce96790bff1fa5eb50c2544cf00f
Luka David <ldavi029@uottawa.ca>
2020-09-24 22:16:45 -0400

    Added graphics into pygame

--- OneDaySimulator.py
+++ OneDaySimulator.py
@@ -4,5 +4,18 @@
 
 import pygame
 
-pygame.init()
+background_colour = (255,255,255)
+(width, height) = (300, 200)
+
+screen = pygame.display.set_mode((width, height))
+
+pygame.display.set_caption('Todays Simulation')
+screen.fill(background_colour)
+pygame.display.flip()
+
+running = True
+while running:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      running = False
 
