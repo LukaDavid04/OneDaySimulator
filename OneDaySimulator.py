30aebc3fdfe9d4a9dbce8b00cf1f9148103370ef
Luka David <ldavi029@uottawa.ca>
2020-10-02 11:04:07 -0400

    Added escape function and set the window consistenty

--- OneDaySimulator.py
+++ OneDaySimulator.py
@@ -2,11 +2,16 @@
 # 'Whatever I feel like simulating on a given day'
 # By: Luka David
 
+
+
+import os
 import pygame
 from pygame.locals import*
 
+os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (300,150)
+
 pygame.init()
-backColour = (0,0,0)
+backColour = (255,255,255)
 (width, height) = (800, 600)
 
 screen = pygame.display.set_mode((width, height))
@@ -18,7 +23,12 @@ pygame.display.flip()
 running = True
 while running:
   for event in pygame.event.get():
+    if event.type == pygame.KEYDOWN:
+      if event.key == pygame.K_ESCAPE:
+        running = False
     if event.type == pygame.QUIT:
       running = False
+
+pygame.quit()
 
 
