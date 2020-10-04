a2529ade5c77e4eb23cd7fe502985214417c7773
Luka David <ldavi029@uottawa.ca>
2020-10-03 22:24:11 -0400

    Added random, and preparing to add GFX

--- OneDaySimulator.py
+++ OneDaySimulator.py
@@ -3,9 +3,9 @@
 # By: Luka David
 
 
-
 import os
 import pygame
+from random import*
 from pygame.locals import*
 
 os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (300,150)
@@ -20,12 +20,22 @@ pygame.display.set_caption('Today\'s Simulation')
 screen.fill(backColour)
 pygame.display.flip()
 
+
+
+def rain():
+  raining = True
+  while raining:
+    a = randint(0, 800)
+
+
 running = True
 while running:
   for event in pygame.event.get():
     if event.type == pygame.KEYDOWN:
       if event.key == pygame.K_ESCAPE:
         running = False
+      if event.key == pygame.K_SPACE:
+        rain()
     if event.type == pygame.QUIT:
       running = False
 
